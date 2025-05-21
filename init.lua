b9b94031d10f27e4dabe970375eac1074cbef3d7
Dmitry Kudryavtsev <dimak@versonix.com>
2025-05-20 18:22:23 -0700

    Add dart lsp, small stuff

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -1011,6 +1011,10 @@ require('lazy').setup({
     },
   },
 })
+require('lspconfig').dartls.setup {
+  cmd = { 'dart', 'language-server', '--protocol=lsp' },
+  filetypes = { 'dart' },
+}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
